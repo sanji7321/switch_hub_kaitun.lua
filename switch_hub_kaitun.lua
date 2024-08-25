2f4848135caed1f8826e566f8e4fa9ee38efc0f8
sanji7321 <ugphonebarato@gmail.com>
2024-08-25 03:06:50 -0300

    Update switch_hub_kaitun.lua

--- switch_hub_kaitun.lua
+++ switch_hub_kaitun.lua
@@ -11,7 +11,7 @@ getgenv().RAM = { -- Log Des
 }
 
 getgenv().Setting = { -- Setting Script / ตั้งค่าสคริปต์
-    ['FPS Booster'] = _G.Setting['FPS Booster'],
+    ['FPS Booster'] = _G.Setting['FPS Booster']
     ['White Screen'] = true,
     ['Disible Gui'] = _G.Setting['Close Ui'],
     ['Lock Fruit'] = 1000000,
